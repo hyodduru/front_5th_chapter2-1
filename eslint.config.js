62cfa6d39a22b4cfff656a7e73573539e6af5619
hyodduru <hoje15v@gmail.com>
2025-04-14 20:59:46 +0900

    chore: ESLint 및 Prettier 설정 파일 추가 및 초기 구성

---
+++ eslint.config.js
@@ -0,0 +1,45 @@
+import js from '@eslint/js';
+import { defineConfig } from 'eslint/config';
+import prettier from 'eslint-config-prettier';
+import plugImport from 'eslint-plugin-import';
+import pluginReact from 'eslint-plugin-react';
+import globals from 'globals';
+
+export default defineConfig([
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    plugins: { js, import: plugImport },
+    extends: ['js/recommended', prettier],
+    rules: {
+      curly: ['error', 'all'],
+      'no-use-before-define': 'error',
+      'no-var': 'error',
+      'prefer-const': 'error',
+      'no-const-assign': 'warn',
+      'import/first': 'error',
+      'import/order': [
+        'warn',
+        {
+          groups: [
+            'builtin',
+            'external',
+            'internal',
+            ['parent', 'sibling', 'index'],
+            'object',
+            'type',
+          ],
+          'newlines-between': 'always',
+          alphabetize: {
+            order: 'asc',
+            caseInsensitive: true,
+          },
+        },
+      ],
+    },
+  },
+  {
+    files: ['**/*.{js,mjs,cjs,jsx}'],
+    languageOptions: { globals: globals.browser },
+  },
+  pluginReact.configs.flat.recommended,
+]);
